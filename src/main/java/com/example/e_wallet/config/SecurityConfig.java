11162eecd7a1977f7a70f0009b2fcbfa50641707
nofield <imadeddine799@gmail.com>
2026-09-17 11:38:04 +0100

    Enable security on methods

--- src/main/java/com/example/e_wallet/config/SecurityConfig.java
+++ src/main/java/com/example/e_wallet/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.e_wallet.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+// enable permission checks on methods like deposit, withdraw, etc..
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
