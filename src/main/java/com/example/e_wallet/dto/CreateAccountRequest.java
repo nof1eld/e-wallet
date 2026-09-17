fb7335b4c6cde91072ed15929ec523823c047b8e
nofield <imadeddine799@gmail.com>
2026-09-17 21:38:29 +0100

    Add create account request DTO

---
+++ src/main/java/com/example/e_wallet/dto/CreateAccountRequest.java
@@ -0,0 +1,6 @@
+package com.example.e_wallet.dto;
+
+// no request data needed because balance starts at 0, and username get extracted from auth token
+public record CreateAccountRequest() {
+    
+}
